f05ed580a31b1c20c8dd36d0bec4458dcf07b93d
Devedu <deveduar@gmail.com>
2024-09-18 21:23:17 +0200

    invetory table with search and pagination, onFilter event with casting for input, viewchild for access to table variable dt2

--- src/app/pages/inventory-table/inventory-table.component.ts
+++ src/app/pages/inventory-table/inventory-table.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { InventoryService, Product } from '../../adapters/api/inventory.service';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { RatingModule } from 'primeng/rating';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { ViewChild } from '@angular/core';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-inventory-table',
   standalone: true,
-  imports: [],
+  imports: [TableModule, TagModule, RatingModule, ButtonModule, CommonModule, FormsModule, InputTextModule, IconFieldModule, InputIconModule],
   templateUrl: './inventory-table.component.html',
   styleUrl: './inventory-table.component.scss'
 })
+
 export class InventoryTableComponent {
+
+  products!: Product[];
+  errorMessage: string | null = null;
+  // rateKey: string = '';
+  loading: boolean = true;
+
+  @ViewChild('dt2') dt2!: Table;
+
+  constructor(private inventoryService: InventoryService){}
+
+  ngOnInit(): void {
+    this.inventoryService.getProducts().subscribe(
+
+      {     
+        next: (data) => {
+        this.products = data;
+        this.errorMessage = null;
+        this.loading = false;
+      },
+        error: (err) => {
+          this.errorMessage = `ERROR: ${err.message}`;
+      }
+      }
+    );
+  };
+
+  onFilter(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement) {
+      this.dt2.filterGlobal(inputElement.value, 'contains')
+    }
+  }
 
 }
