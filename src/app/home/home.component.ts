d432e0122d86d29b21b4b53a6bd5b6d4942ec8f0
Devedu <deveduar@gmail.com>
2024-09-26 01:36:12 +0200

    product picker in separated component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { InventoryTableComponent } from '../pages/inventory-table/inventory-table.component';
-import { OrderListModule } from 'primeng/orderlist';
+import { ProductPickerComponent } from '../pages/product-picker/product-picker.component';
 import { InventoryService } from '../adapters/api/inventory.service';
 import { Product } from '../domain/models/product';
-import { ListboxModule } from 'primeng/listbox';
 import { ProductDetailComponent } from '../pages/product-detail/product-detail.component';
-import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -15,12 +11,8 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [
     CommonModule,
-    ButtonModule,
-    InventoryTableComponent,
-    OrderListModule,
-    ListboxModule,
-    ProductDetailComponent, FormsModule
-  
+    ProductDetailComponent, 
+    ProductPickerComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -28,7 +20,6 @@ import { FormsModule } from '@angular/forms';
 export class HomeComponent {
   products!: Product[];
   errorMessage: string | null = null;
-  selectedProduct!: Product;
 
   constructor(private inventoryService: InventoryService){}
 
@@ -39,11 +30,6 @@ export class HomeComponent {
         next: (data) => {
         this.products = data.slice(0, 20);
         this.errorMessage = null;
-        // console.log(data)
-        if (this.products.length > 0) {
-          this.selectedProduct = this.products[0];  
-          this.onProductSelect({ value: this.selectedProduct }); 
-        }
       },
         error: (err) => {
           this.errorMessage = `ERROR: ${err.message}`;
@@ -51,8 +37,4 @@ export class HomeComponent {
       }
     );
   };
-
-  onProductSelect(event: any) {
-    this.selectedProduct = event.value;
-  }
 }
---
+++ src/app/pages/product-picker/product-picker.component.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
+import { OrderListModule } from 'primeng/orderlist';
+import { InventoryService } from '../../adapters/api/inventory.service';
+import { Product } from '../../domain/models/product';
+import { ListboxModule } from 'primeng/listbox';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { FormsModule } from '@angular/forms'
+
+
+@Component({
+  selector: 'app-product-picker',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ButtonModule,
+    OrderListModule,
+    ListboxModule,
+    ProductDetailComponent, 
+    FormsModule
+  
+  ],
+  templateUrl: './product-picker.component.html',
+  styleUrl: './product-picker.component.scss'
+})
+export class ProductPickerComponent {
+  products!: Product[];
+  errorMessage: string | null = null;
+  selectedProduct!: Product;
+
+  constructor(private inventoryService: InventoryService){}
+
+  ngOnInit(): void {
+    this.inventoryService.getProducts().subscribe(
+
+      {     
+        next: (data) => {
+        this.products = data.slice(0, 20);
+        this.errorMessage = null;
+        // console.log(data)
+        if (this.products.length > 0) {
+          this.selectedProduct = this.products[0];  
+          this.onProductSelect({ value: this.selectedProduct }); 
+        }
+      },
+        error: (err) => {
+          this.errorMessage = `ERROR: ${err.message}`;
+      }
+      }
+    );
+  };
+
+  onProductSelect(event: any) {
+    this.selectedProduct = event.value;
+  }
+}
