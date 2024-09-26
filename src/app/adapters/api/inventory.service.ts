fac8632692d2f28c97636355525beb5207e8aefe
Devedu <deveduar@gmail.com>
2024-09-26 03:39:49 +0200

    product details with router with id and inyect, control conection error for product picker

--- src/app/adapters/api/inventory.service.ts
+++ src/app/adapters/api/inventory.service.ts
@@ -10,12 +10,19 @@ import { Product } from '../../domain/models/product';
 
 export class InventoryService {
   private apiUrl = 'https://fakestoreapi.com/products?limit=20';
-
+  private productDetailUrl = 'https://fakestoreapi.com/products'; 
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl)
   }
+
+  getProductById(id: number): Observable<Product> {
+    const url = `${this.productDetailUrl}/${id}`;
+    return this.http.get<Product>(url);
+  }
+
+
 
 }
--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../domain/models/product';
+import { InventoryService } from '../../adapters/api/inventory.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,7 +11,19 @@ import { Product } from '../../domain/models/product';
   styleUrl: './product-detail.component.scss'
 })
 export class ProductDetailComponent {
-  @Input() product!: Product;
+  @Input() product?: Product;
 
+  constructor(private route: ActivatedRoute, private inventoryService: InventoryService) {}
+  
+  ngOnInit(): void {
+    if (!this.product) {
+      const productId = this.route.snapshot.paramMap.get('id');
+      if (productId) {
+        this.inventoryService.getProductById(+productId).subscribe((product: Product) => {
+          this.product = product;
+        });
+      }
+    }
+  }
 
 }
